fed55792562376e67ff985f30e10893d113cd8ae
Apehum <apehum@proton.me>
2026-04-25 06:47:31 +0800

    call original setSoundVolume when audio render is disabled

--- src/main/java/com/apehum/replayaudio/mixin/MixinVideoRenderer.java
+++ src/main/java/com/apehum/replayaudio/mixin/MixinVideoRenderer.java
@@ -1,16 +1,21 @@
 package com.apehum.replayaudio.mixin;
 
+import com.apehum.replayaudio.AudioRenderSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.replaymod.render.rendering.VideoRenderer;
 import net.minecraft.client.OptionInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(VideoRenderer.class)
 public class MixinVideoRenderer {
-
-    @Redirect(method = "setup", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/OptionInstance;set(Ljava/lang/Object;)V"))
-    private void setSoundVolume(OptionInstance<?> instance, Object object) {
-        // do nothing
+    @WrapOperation(
+            method = "setup",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/OptionInstance;set(Ljava/lang/Object;)V")
+    )
+    private void setSoundVolume(OptionInstance<?> instance, Object object, Operation<Void> original) {
+        if (AudioRenderSettings.get().enabled) return;
+        original.call(instance, object);
     }
 }
