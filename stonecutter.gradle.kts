f48eb0a0edf9c3098b01f5ab78e0f900b72d344e
Apehum <apehum@proton.me>
2026-04-27 06:58:02 +0800

    sort publish tasks by project name
    [ci skip]

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -2,3 +2,12 @@ plugins {
     id("dev.kikugie.stonecutter")
 }
 stonecutter active "1.19.3"
+
+gradle.projectsEvaluated {
+    subprojects.sortedBy { it.name }.zipWithNext { prev, curr ->
+        curr.tasks.matching { it.name.startsWith("publish") }.configureEach {
+            val taskName = name
+            mustRunAfter(prev.tasks.matching { it.name == taskName })
+        }
+    }
+}
